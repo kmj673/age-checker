beec91184606691871b7931e6df1ebfb1bf30a99
Minju Kim <kmj673@gmail.com>
2022-03-14 22:54:07 +0000

    added number or not checker
    Relates #3

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 const form = document.querySelector("#age");
 
-function handleForm(e) {
+function handleAge(e) {
   e.preventDefault();
-  console.log(e);
+  let formData = new FormData(form);
+  let data = Object.fromEntries(formData);
+  let age = data.age;
+  return age;
 }
 
-form.addEventListener("submit", handleForm);
+function checkNumber(age) {
+  let intAge = parseInt(age);
+  let notANumber = Number.isNaN(intAge);
+  let numberOrNot = notANumber ? false : true;
+  return numberOrNot;
+}
+
+function calculate(age) {
+  let eighteenOlder = age >= 18 ? true : false;
+  console.log(eighteenOlder);
+}
+
+form.addEventListener("submit", (e) => {
+  let age = handleAge(e);
+  console.log(checkNumber(age));
+  if (checkNumber(age)) {
+    calculate(age);
+  } else {
+    console.log(`enter valid number as age`);
+  }
+});
